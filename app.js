a85a2a20bba78f249913152641fe0befe7d737fe
AbdulSelimShaikh <abdulselimsh26@gmail.com>
2024-09-18 01:37:43 +0530

    Update project with MongoDB connection and Netlify configuration

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ const app = express();
 require("dotenv").config();
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB Atlas"))
   .catch((err) => console.error("Error connecting to MongoDB Atlas:", err));
 
